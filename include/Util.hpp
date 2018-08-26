be6d582599604ac560a150820d74aed03e4bd71d
Resolt <philip@neuronsinc.com>
2018-08-26 17:34:40 +0200

    -added basics

---
+++ include/Util.hpp
@@ -0,0 +1,14 @@
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
+#include <iostream>
+
+namespace util
+{
+	void sayHi()
+	{
+		std::cout << "Hi" << std::endl << std::endl;
+	}
+}
+
+#endif /* UTIL_HPP */
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Util.hpp"
+
+int main()
+{
+	util::sayHi();
+
+	return 0;
+}
